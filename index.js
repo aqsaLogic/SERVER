ebd88198f65a67e8ea190355badb83dea07af6d5
aqsa-eng <nmalbaig@gmail.com>
2026-05-17 10:21:18 -0700

    push changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://cozy-alfajores-eeb14a.netlify.app/login',  
+  origin: 'https://cozy-alfajores-eeb14a.netlify.app',  
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
 
